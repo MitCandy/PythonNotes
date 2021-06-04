5575705d9ff5a4288a4a15ed3211f8f1eed403c3
MitCandy <85299940+MitCandy@users.noreply.github.com>
2021-06-04 09:44:04 +0700

    Create numbers.py

---
+++ Basics/numbers.py
@@ -0,0 +1,25 @@
+# SimpleNumber
+print(3)
+
+# Decimals n negatives???
+print(2.4)
+print(-3.4)
+
+# Arithmetical operation
+print(2 + 5)
+print(2 - 1.2)
+print(20 / 5)
+print(9 * 3)
+print(10 % 3)
+
+# Convert int to str if want to combine with strings
+my_num = 22
+print("Number: " + str(my_num))
+
+# Another functions
+my_num = -6
+print(abs(my_num)) # absolute
+print(pow(5, 2)) # power / 5^2
+print(max(5, 2)) # shows bigger number
+print(pow(5, 2)) # vise versa
+print(round(2.4)) # round a number
